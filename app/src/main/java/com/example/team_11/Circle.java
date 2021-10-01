220d45cee18c38b6ad91be8bfc3405ee871a0f1e
vivek-rgb <vivek-rgb@github.com>
2021-10-01 17:10:36 -0600

    Circle class extends Shape

---
+++ app/src/main/java/com/example/team_11/Circle.java
@@ -0,0 +1,10 @@
+package com.example.team_11;
+
+// Vivek's shape subclass contribution
+public class Circle extends Shape{
+    private int radius;
+
+    public Circle(int x){
+        this.radius = x;
+    }
+}
