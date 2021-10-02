8be7aec835e3b1416669b601a76a43cb78d0588b
Kelly Shih <kshih@ualberta.ca>
2021-10-01 19:09:03 -0600

    Add star

--- app/src/main/java/com/example/team_11/Star.java
+++ app/src/main/java/com/example/team_11/Star.java
@@ -1,4 +1,12 @@
 package com.example.team_11;
 
-public class Star {
+//Kelly's Shape subclass contribution
+
+public class Star extends Shape {
+
+    private int angle;
+
+    public Star(int angle) {
+        this.angle = angle;
+    }
 }
