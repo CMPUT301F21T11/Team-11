dcf9482d7813da79d281b050794f94d96ea90519
Rehamalbakouni <77491063+Rehamalbakouni@users.noreply.github.com>
2021-10-03 22:55:32 -0600

    Update Shape.java
    resolve conflict

--- app/src/main/java/com/example/team_11/Shape.java
+++ app/src/main/java/com/example/team_11/Shape.java
@@ -3,6 +3,7 @@ package com.example.team_11;
 public abstract class Shape {
     int x,y;
     String colorV = "blue";
+    String colorA = "red";
     String colorR = "blue";
     String color = "ROYGBIV";
 }
