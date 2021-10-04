e2c4a04b29e6ac9d18a579600a2988f35b8c2b43
Rehamalbakouni <77491063+Rehamalbakouni@users.noreply.github.com>
2021-10-03 20:36:48 -0600

    Update Shape.java

--- app/src/main/java/com/example/team_11/Shape.java
+++ app/src/main/java/com/example/team_11/Shape.java
@@ -2,14 +2,7 @@ package com.example.team_11;
 
 public abstract class Shape {
     int x,y;
-<<<<<<< HEAD
-    String color = "blue";
-    
-=======
-	
     String colorV = "blue";
     string colorR = "blue";
     String color = "ROYGBIV";
-
->>>>>>> 1fa2813b180f720e5277088b2f850115f7d871b6
 }
