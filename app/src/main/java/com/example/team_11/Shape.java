4a546fb18ef6a40c234991a2120f20d2ba6928cc
Courtenay Laing-Kobe <laingkob@ualberta.ca>
2021-10-04 11:04:00 -0600

    Fixed file type

--- app/src/main/java/com/example/team_11/Shape.java
+++ app/src/main/java/com/example/team_11/Shape.java
@@ -2,8 +2,5 @@ package com.example.team_11;
 
 public abstract class Shape {
     int x,y;
-    String colorV = "blue";
-    String colorA = "red";
-    String colorR = "blue";
-    String color = "yellow";
+
 }
